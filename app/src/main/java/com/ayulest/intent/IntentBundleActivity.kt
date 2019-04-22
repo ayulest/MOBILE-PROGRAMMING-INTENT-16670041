289d0a918567c6fc14afdb4e1baf64d61bfce2b5
ayulest <ayyu.lest6@gmail.com>
2019-04-22 20:59:43 +0700

    update #4

--- app/src/main/java/com/ayulest/intent/IntentBundleActivity.kt
+++ app/src/main/java/com/ayulest/intent/IntentBundleActivity.kt
@@ -1,30 +1,81 @@
 package com.ayulest.intent
 
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_intent_bundle.*
+import android.content.Intent
+import android.content.ActivityNotFoundException
+import android.net.Uri
+
 
 class IntentBundleActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        requestWindowFeature(1);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_intent_bundle)
+        btnInstagram.setOnClickListener {
+            val uri = Uri.parse("http://instagram.com/_u/ayle0699")
+            val likeIng = Intent(Intent.ACTION_VIEW, uri)
 
-        val nama = intent.getStringExtra("Nama")
-        val ttl = intent.getStringExtra("TTL")
-        val alamat = intent.getStringExtra("Alamat")
-        val hobby = intent.getStringExtra(" Hobby")
-        val agama = intent.getStringExtra("Agama")
-        val email = intent.getStringExtra("Email")
+            likeIng.setPackage("com.instagram.android")
 
-        tvnama.text = nama
-        tvttl.text = ttl
-        tvalamat.text = alamat
-        tvhobby.text = hobby
-        tvagama.text = agama
-        tvemail.text = email
+            try {
+                startActivity(likeIng)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://instagram.com/ayle0699")
+                    )
+                )
+            }
 
+        }
+        btnYoutube.setOnClickListener {
+            val uri = Uri.parse("https://www.youtube.com/channel/UCzgxx_DM2Dcb9Y1spb9mUJA")
+            val likeIng = Intent(Intent.ACTION_VIEW, uri)
 
+            likeIng.setPackage("com.google.android.youtube")
 
+            try {
+                startActivity(likeIng)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://www.youtube.com/channel/UCzgxx_DM2Dcb9Y1spb9mUJA")
+                    )
+                )
+            }
+
+        }
+//        val nama = intent.getStringExtra("NAMA")
+//        val ttl = intent.getStringExtra("NPM")
+//        val alamat = intent.getStringExtra("SEKOLAH")
+//        val hobby = intent.getStringExtra("HOBBY")
+//        val agama = intent.getStringExtra("AGAMA")
+//        val email = intent.getStringExtra("JK")
+//
+//        tvnama.text = nama
+//        tvttl.text = ttl
+//        tvalamat.text = alamat
+//        tvhobby.text = hobby
+//        tvagama.text = agama
+//        tvemail.text = email
     }
 }
--- app/src/main/java/com/ayulest/intent/MainActivity.kt
+++ app/src/main/java/com/ayulest/intent/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ayulest.intent
 
+import android.Manifest
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kotlinpermissions.KotlinPermissions
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        KotlinPermissions.with(this) // where this is an FragmentActivity instance
+            .permissions(
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//                Manifest.permission.ACCESS_FINE_LOCATION,
+//                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.CAMERA
+//                Manifest.permission.SEND_SMS,
+//                Manifest.permission.CALL_PHONE
+            )
+            .onAccepted { permissions ->
+                Toast.makeText(this, "Accesed" + BuildConfig.APPLICATION_ID, Toast.LENGTH_LONG).show()
+            }
+            .onDenied { permissions ->
+                Toast.makeText(this, "Denied", Toast.LENGTH_LONG).show()
+            }
+            .onForeverDenied { permissions ->
+                //List of forever denied permissions
+            }
+            .ask()
         btnPindahAcrivityExplisit.setOnClickListener {
           startActivity(Intent(this,ExplisitIntentActivity::class.java))
         }
@@ -21,7 +43,8 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("NAMA", "Ayu")
             intent.putExtra("NPM", "16670041")
             intent.putExtra("SEKOLAH", "Universitas PGRI Semarang")
-            intent.putExtra("FOTO_URL", "https://cdnx.kincir.com/production/media/2018/november/5-webtoon-yang-ditunggu-adaptasi-serial-dramanya/1-judul-webtoon-yang-ditunggu-adaptasi-serial-dramanya.jpg")
+            intent.putExtra("HOBBY", "Watching Drama")
+            intent.putExtra("AGAMA", "Islam")
             intent.putExtra("FOTO", android.R.drawable.alert_light_frame)
             intent.putExtra("JK", "Girls")
             startActivity(intent)
